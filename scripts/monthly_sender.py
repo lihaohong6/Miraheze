30823a364bcbd059715f0bfcfd068562b8281428
Peter Li <lihaohong6@gmail.com>
2025-09-01 23:48:33 -0400

    monthly sender: print pings as well

--- scripts/monthly_sender.py
+++ scripts/monthly_sender.py
@@ -7,10 +7,10 @@ from wikitextparser import parse
 from utils.general_utils import meta
 
 
-def parse_links(page: Page) -> list[str]:
+def parse_links(page: Page, section_title: str) -> list[str]:
     parsed = parse(page.text)
     for section in parsed.sections:
-        if section.title and "talk" in section.title.lower():
+        if section.title and section_title in section.title.lower():
             break
     else:
         return []
@@ -35,23 +35,40 @@ def main():
                         type=str,
                         default="Automated delivery of [[User:Raidarr/Miraheze Monthly|Miraheze Monthly]]")
     args = parser.parse_args()
-    page = Page(meta(), args.page)
-    talk_pages = [Page(meta(), title) for title in parse_links(page)]
+    subscribers_page = Page(meta(), args.page)
+    deliver_to_talk_page(subscribers_page, date=args.date, summary=args.summary)
+    print_pings(subscribers_page)
+
+
+def print_pings(page: Page) -> None:
+    user_pages = [Page(meta(), title)
+                  for title in parse_links(page, section_title="notification subscriptions")]
+    user_pages = list(PreloadingGenerator(user_pages))
+    for p in user_pages:
+        if p.namespace().id != 2:
+            print(f"Error: {p.title()} is not in user NS.")
+            continue
+    print("{{ping|" + "|".join(p.title(with_ns=False) for p in user_pages) + "}}")
+
+
+def deliver_to_talk_page(page: Page, date: str, summary: str) -> None:
+    talk_pages = [Page(meta(), title) for title in parse_links(page, "talk")]
     talk_pages = list(PreloadingGenerator(talk_pages))
     for p in talk_pages:
         if p.namespace().id != 3:
             print(f"Warning: {p.title()} is not in user talk NS.")
-    date = args.date
     text = f"""==Miraheze Monthly - [[User:Raidarr/Miraheze Monthly/{date}|{date} issue]]==
 {{{{:User:Raidarr/Miraheze Monthly/{date}}}}}
 <div style="text-align: right"><span style="display: none">[[User:%s]]</span>Delivered by ~~~~</div>"""
     for page in talk_pages:
         if page.text.strip() != "":
             page.text = page.text.rstrip() + "\n\n"
+        if text.split("\n")[0] in page.text:
+            print(f"Warning: {page.title()} already contains the current MH Monthly issue.")
+            continue
         username = page.title().split("/")[0].split(":")[1]
         user_message = text.replace("%s", username)
         page.text += user_message
-        summary = args.summary
         page.save(summary=summary)
 
 
