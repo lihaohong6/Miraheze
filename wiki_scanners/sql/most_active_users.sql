a30b1155bac4443a3beb0f711ccf3d4669dec7db
Peter Li <lihaohong6@gmail.com>
2025-09-15 20:28:59 -0400

    wiki scanner: add sql file for a common query

---
+++ wiki_scanners/sql/most_active_users.sql
@@ -0,0 +1,11 @@
+select all_wikis.db_name as db,
+       all_wikis.site_name,
+       CAST(coalesce(json_extract(wiki_extensions.data, '$.settings.wgActiveUserDays'), 30) as integer)
+                         as active_user_days,
+       json_extract(wiki_statistics.data, '$.active_users')
+                         as active_users
+from wiki_statistics
+         join all_wikis on wiki_statistics.db_name = all_wikis.db_name
+         join wiki_extensions on all_wikis.db_name = wiki_extensions.db_name
+where active_users > 10
+order by active_users DESC;
