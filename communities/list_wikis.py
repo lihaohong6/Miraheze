5c29902cade19e252134544088ba7a5e3ef924c5
Peter Li <lihaohong6@gmail.com>
2025-09-24 02:14:14 -0400

    communities: add airium for html generation; create one more wiki list

--- communities/list_wikis.py
+++ communities/list_wikis.py
@@ -1,36 +1,64 @@
+from functools import cache
+
+from airium import Airium
+
+from communities.update_wiki_stats import get_all_stats
 from communities.wiki_list import get_item_id_from_wiki
+from utils.general_utils import MirahezeWiki
 from utils.wiki_scanner import fetch_all_mh_wikis, run_wiki_scanner_query
 
+@cache
+def get_wiki_dict() -> dict[str, MirahezeWiki]:
+    return dict((w.db_name, w) for w in fetch_all_mh_wikis())
+
+def get_wiki_name_column(db: str) -> str:
+    wiki = get_wiki_dict()[db]
+    item_id = get_item_id_from_wiki(wiki)
+    if item_id is None:
+        item = ""
+    else:
+        item = f" ([[Item:{item_id}|{item_id}]])"
+    return f"[[mh:{db[:-4]}|{wiki.site_name}]]{item}"
+
+def list_wikis_by_article_count():
+    a = Airium(base_indent="")
+    with a.table(klass="wikitable sortable"):
+        with a.tr():
+            a.th(_t="Name")
+            a.th(_t="Article count")
+            a.th(_t="Page count")
+
+        for wiki in run_wiki_scanner_query("most_articles")[:500]:
+            db, name, articles, pages = wiki
+            with a.tr():
+                a.td(_t=get_wiki_name_column(db))
+                a.td(_t=str(articles))
+                a.td(_t=str(pages))
+    print(str(a))
 
 def list_wikis_by_active_users():
-    wikis = dict((w.db_name, w) for w in fetch_all_mh_wikis())
-    print('<table class="wikitable sortable">')
-    print('<tr>')
-    print('<th>Name</th><th>Active users</th><th style="word-break: break-word">wgActiveUserDays</th>')
-    print('</tr>')
-    for wiki in run_wiki_scanner_query("most_active_users")[:800]:
-        db, name, au_days, active_users = wiki
-        if active_users < 4:
-            continue
-        item_id = get_item_id_from_wiki(wikis[db])
-        if item_id is None:
-            item = ""
-        else:
-            item = f" ([[Item:{item_id}|{item_id}]])"
-        lines = [
-            "<tr>",
-            f"<td>[[mh:{db[:-4]}|{name}]]{item}</td>"
-        ]
-        au_days = int(au_days)
-        style = ""
-        if au_days != 30:
-            style = 'style="background-color: var(--background-color-disabled, #dadde3)"'
-        lines.append(f"<td {style}>{active_users}</td>")
-        lines.append(f"<td>{'-' if au_days == 30 else au_days}</td>")
-        lines.append(f"</tr>")
-        print("\n".join(lines))
-    print("</table>")
+    a = Airium(base_indent="")
+    with a.table(klass="wikitable sortable"):
+        with a.tr():
+            a.th(_t="Name")
+            a.th(_t="Active users")
+            a.th(_t="wgActiveUserDays", style="word-break: break-word")
+
+        for wiki in run_wiki_scanner_query("most_active_users")[:800]:
+            db, name, au_days, active_users = wiki
+            if active_users < 4:
+                continue
+            with a.tr():
+                a.td(_t=get_wiki_name_column(db))
+                style = 'background-color: var(--background-color-disabled, #dadde3)'
+                au_days = int(au_days)
+                if au_days != 30:
+                    a.td(_t=str(active_users), style=style)
+                else:
+                    a.td(_t=str(active_users))
+                a.td(_t=str('-' if au_days == 30 else au_days))
+    print(str(a))
 
 
 if __name__ == '__main__':
-    list_wikis_by_active_users()
+    list_wikis_by_article_count()
