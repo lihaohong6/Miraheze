5b1ff2a7459b99df68b4ffc5e428c35d3a2fc071
Peter Li <lihaohong6@gmail.com>
2025-10-05 17:00:54 -0400

    SSL: improve custom domain scanner

--- scripts/custom_domains_scanner.py
+++ scripts/custom_domains_scanner.py
@@ -1,4 +1,5 @@
 import re
+from itertools import dropwhile
 from time import sleep
 
 import requests
@@ -11,6 +12,7 @@ def main():
     r = requests.get("https://raw.githubusercontent.com/miraheze/ssl/refs/heads/main/wikidiscover_output.yaml")
     r.raise_for_status()
     text = r.text
+    urls = []
     for line in text.splitlines():
         line = line.strip()
         if line == "":
@@ -20,6 +22,10 @@ def main():
             print(f"ERROR: {line} does not seem to contain a custom domain")
             continue
         url = re_match.group(1)
+        urls.append(url)
+    # Uncomment to resume
+    # urls = list(dropwhile(lambda x: "gimkit" not in x, urls))
+    for url in urls:
         test_custom_domain(url)
         sleep(0.5)
 
@@ -30,7 +36,7 @@ def test_custom_domain(url, retry_cloudflare: int = 2) -> None:
         response= response.text
     except Exception as e:
         error_text = str(e)
-        if "NameResolutionError" in e:
+        if "NameResolutionError" in error_text:
             logger.error(f"{url} has expired: NameResolutionError")
         else:
             logger.error(f"{url} may have expired: {error_text}")
@@ -45,11 +51,14 @@ def test_custom_domain(url, retry_cloudflare: int = 2) -> None:
             else:
                 logger.error(f"got cloudflared twice for {url}")
             return
-        if response.strip() == "":
-            logger.warning(f"{url} may have expired")
+        if "expired" in response.lower():
+            if "namecheap" in response.lower():
+                logger.warning(f"{url} has expired: namecheap")
+            else:
+                logger.warning(f"{url} has expired")
         else:
             logger.warning(f"{url} may no longer be a mediawiki site")
-            logger.warning(response[:100].replace("\n", ""))
+            logger.info(response[:1000].replace("\n", ""))
         return
     if "footer-mirahezeico" not in response and "Hosted by Miraheze" not in response and "meta.miraheze.org" not in response:
         logger.error(f"{url} is no longer with us")
