fafc63a48669fac7c7ae30a9a90a88cf1e6feabd
Peter Li <lihaohong6@gmail.com>
2025-11-03 02:13:01 -0500

    communities: be more verbose when updating everything

--- communities/update_everything.py
+++ communities/update_everything.py
@@ -6,10 +6,15 @@ from wiki_scanners.analyses import force_update_all_statistics
 
 
 def main():
+    print("All stats.")
     force_update_all_statistics()
+    print("Local db.")
     update_local_db()
+    print("Wikibase")
     update_all_wikibase_pages()
+    print("Wiki list")
     update_wiki_list_pages()
+    print("Wiki rank")
     rank_wikis()
 
 
